e6484858dd9cfc42e8c4446ac1888995e74960b5
p848332 <p848332@8451.com>
2024-01-15 20:00:45 -0500

    more reorg

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use inquire::{validator::Validation, InquireError, Select, Text};
-
 use std::env;
 use std::ffi::OsStr;
 use std::fs::{create_dir, create_dir_all, read_dir, remove_dir_all, remove_file, File};
 use std::path::PathBuf;
-use std::process::exit;
 
-use crate::prompts::{prompt_for_action, prompt_for_new_note_name, prompt_for_note};
+use crate::prompts::{
+    confirm_delete, prompt_for_action, prompt_for_new_note_name, prompt_for_note,
+    prompt_for_project, prompt_for_project_name,
+};
 use crate::structs::{Action, Config, Note, Project};
 
 mod prompts;
 mod structs;
-
 
 /// Returns if the root dir exists already
 ///
@@ -45,7 +44,7 @@ fn create_root_folder(config: &Config) {
 fn create_objects(config: &Config) -> (Vec<Note>, Vec<Project>) {
     let mut notes: Vec<Note> = Vec::new();
     let mut projects: Vec<Project> = Vec::new();
-    _get_dir_objects(
+    get_dir_objects(
         &config.root_dir,
         &mut notes,
         &mut projects,
@@ -64,7 +63,7 @@ fn create_objects(config: &Config) -> (Vec<Note>, Vec<Project>) {
 /// * `projects` - The current state of a vector of projects to append to
 /// * `root_dir` - the overall root_dir of the run
 /// * `ignore_dirs` - any directories that should be ignored, like .git dirs
-fn _get_dir_objects(
+fn get_dir_objects(
     base: &PathBuf,
     notes: &mut Vec<Note>,
     projects: &mut Vec<Project>,
@@ -94,7 +93,7 @@ fn _get_dir_objects(
             }
             let curr_project = Project { trunc_path };
             projects.push(curr_project);
-            _get_dir_objects(&curr_path, notes, projects, root_dir, ignore_dirs);
+            get_dir_objects(&curr_path, notes, projects, root_dir, ignore_dirs);
         } else {
             let curr_note = Note { trunc_path };
             notes.push(curr_note)
@@ -133,26 +132,6 @@ fn create_new_note(config: &Config, orig_note_name: String, project_path: PathBu
     return note_path;
 }
 
-/// Prompts the user for a note to take action on
-///
-/// # Arguments
-///
-/// * `notes` - a reference to the notes vector
-/// * `action` - an action to take, only used to prompt the user
-fn prompt_for_project(project: &Vec<Project>, action: String) -> PathBuf {
-    let options = project
-        .iter()
-        .map(|note| note.trunc_path.to_str().unwrap())
-        .collect();
-    let prompt = String::from("What project would you like to ") + &action + &String::from("?");
-    let ans: Result<&str, InquireError> = Select::new(&prompt, options).with_page_size(20).prompt();
-
-    match ans {
-        Ok(choice) => return PathBuf::from(choice.trim()),
-        Err(_) => panic!("There was an error, please try again"),
-    }
-}
-
 /// Creates a new project directory
 ///
 /// # Arguments
@@ -168,37 +147,6 @@ fn create_new_project(config: &Config, project_name: String) {
     }
     let _ = create_dir(&project_path);
     println!("New project created: {}", project_name);
-}
-
-/// Confirms with the user that they want a file to be deleted
-///
-/// # Arguments
-///
-/// * `path` - the potential file path to delete
-fn confirm_delete(path: &PathBuf, full_path: &PathBuf) {
-    if full_path.is_dir() {
-        println!(
-            "{} is a project, so this will delete all child notes as well, be VERY careful!",
-            path.display()
-        );
-    }
-    let options = vec!["Yes", "No"];
-    let prompt = String::from("Are you sure you want to delete ")
-        + &path.to_string_lossy()
-        + &String::from("?");
-    let ans: Result<&str, InquireError> = Select::new(&prompt, options)
-        .with_starting_cursor(1)
-        .prompt();
-
-    match ans {
-        Ok(input) => {
-            if input.trim() == "No" {
-                println!("Cancelling ...");
-                exit(0);
-            }
-        }
-        Err(_) => panic!("There was an error, please try again"),
-    }
 }
 
 /// Deletes the passed PathBuf
@@ -224,37 +172,6 @@ fn delete(full_path: PathBuf) -> bool {
     }
 
     return true;
-}
-
-/// Prompts the user for a valid project name
-fn prompt_for_project_name() -> String {
-    let validator = |name: &str| {
-        let invalid_chars = vec![
-            '/', '\\', '"', '\'', '*', ';', '-', '?', '[', ']', '(', ')', '~', '!', '$', '{', '}',
-            '<', '>', '#', '@', '&', '|', ' ',
-        ];
-        if name
-            .chars()
-            .into_iter()
-            .any(|curr| invalid_chars.contains(&curr))
-        {
-            return Ok(Validation::Invalid(
-                "Name contains invalid character".into(),
-            ));
-        } else if name.len() == 0 {
-            return Ok(Validation::Invalid("Name is length zero".into()));
-        } else {
-            return Ok(Validation::Valid);
-        }
-    };
-    let name = Text::new("What would you like to name this project?")
-        .with_validator(validator)
-        .prompt();
-
-    match name {
-        Ok(name) => return name,
-        Err(_) => panic!("An error happened when asking for your project, try again later."),
-    }
 }
 
 fn main() {
--- src/prompts.rs
+++ src/prompts.rs
@@ -1,8 +1,9 @@
 use std::path::PathBuf;
+use std::process::exit;
 
 use inquire::{validator::Validation, InquireError, Select, Text};
 
-use crate::structs::{Action, Note};
+use crate::structs::{Action, Note, Project};
 
 /// Prompts the user for the action they want to take
 pub fn prompt_for_action() -> Action {
@@ -80,5 +81,87 @@ pub fn prompt_for_note(notes: &Vec<Note>, action: String) -> PathBuf {
     match ans {
         Ok(choice) => return PathBuf::from(choice.trim()),
         Err(_) => panic!("There was an error, please try again"),
+    }
+}
+
+/// Prompts the user for a note to take action on
+///
+/// # Arguments
+///
+/// * `notes` - a reference to the notes vector
+/// * `action` - an action to take, only used to prompt the user
+pub fn prompt_for_project(project: &Vec<Project>, action: String) -> PathBuf {
+    let options = project
+        .iter()
+        .map(|note| note.trunc_path.to_str().unwrap())
+        .collect();
+    let prompt = String::from("What project would you like to ") + &action + &String::from("?");
+    let ans: Result<&str, InquireError> = Select::new(&prompt, options).with_page_size(20).prompt();
+
+    match ans {
+        Ok(choice) => return PathBuf::from(choice.trim()),
+        Err(_) => panic!("There was an error, please try again"),
+    }
+}
+
+/// Confirms with the user that they want a file to be deleted
+///
+/// # Arguments
+///
+/// * `path` - the potential file path to delete
+pub fn confirm_delete(path: &PathBuf, full_path: &PathBuf) {
+    if full_path.is_dir() {
+        println!(
+            "{} is a project, so this will delete all child notes as well, be VERY careful!",
+            path.display()
+        );
+    }
+    let options = vec!["Yes", "No"];
+    let prompt = String::from("Are you sure you want to delete ")
+        + &path.to_string_lossy()
+        + &String::from("?");
+    let ans: Result<&str, InquireError> = Select::new(&prompt, options)
+        .with_starting_cursor(1)
+        .prompt();
+
+    match ans {
+        Ok(input) => {
+            if input.trim() == "No" {
+                println!("Cancelling ...");
+                exit(0);
+            }
+        }
+        Err(_) => panic!("There was an error, please try again"),
+    }
+}
+
+/// Prompts the user for a valid project name
+pub fn prompt_for_project_name() -> String {
+    let validator = |name: &str| {
+        let invalid_chars = vec![
+            '/', '\\', '"', '\'', '*', ';', '-', '?', '[', ']', '(', ')', '~', '!', '$', '{', '}',
+            '<', '>', '#', '@', '&', '|', ' ',
+        ];
+        if name
+            .chars()
+            .into_iter()
+            .any(|curr| invalid_chars.contains(&curr))
+        {
+            return Ok(Validation::Invalid(
+                "Name contains invalid character".into(),
+            ));
+        } else if name.len() == 0 {
+            return Ok(Validation::Invalid("Name is length zero".into()));
+        } else {
+            return Ok(Validation::Valid);
+        }
+    };
+    let name = Text::new("What would you like to name this project?")
+        .with_validator(validator)
+        .prompt();
+
+    match name {
+        Ok(name) => return name,
+        Err(_) => panic!("An error happened when asking for your project, try again later."),
     }
 }
