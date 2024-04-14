1668aaaa329cf51f069a22cc8384bbb277ba081e
Parker Kain <parkerjkain@gmail.com>
2024-04-14 19:36:25 -0400

    search bar ui

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,5 +1,32 @@
-use ratatui::Frame;
+use ratatui::{
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    text::Text,
+    widgets::{Block, Borders, Paragraph},
+    Frame,
+};
 
 use super::app::App;
 
-pub fn ui(f: &mut Frame, app: &App) {}
+pub fn search_ui(f: &mut Frame) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(3), Constraint::Min(1)])
+        .split(f.size());
+
+    let title_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let title = Paragraph::new(Text::styled(
+        "Search for a note",
+        Style::default().fg(Color::Green),
+    ))
+    .block(title_block);
+
+    f.render_widget(title, chunks[0])
+}
+
+pub fn ui(f: &mut Frame, app: &App) {
+    search_ui(f);
+}
