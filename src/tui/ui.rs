774bfc2f84859be555e2b1e7fea26232a115b784
Parker Kain <parkerjkain@gmail.com>
2024-04-14 19:45:08 -0400

    holy guacamole it's a thing

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,5 +1,7 @@
+use std::rc::Rc;
+
 use ratatui::{
-    layout::{Constraint, Direction, Layout},
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style},
     text::Text,
     widgets::{Block, Borders, Paragraph},
@@ -8,12 +10,7 @@ use ratatui::{
 
 use super::app::App;
 
-pub fn search_ui(f: &mut Frame) {
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Length(3), Constraint::Min(1)])
-        .split(f.size());
-
+pub fn search_ui(f: &mut Frame, chunks: &Rc<[Rect]>) {
     let title_block = Block::default()
         .borders(Borders::ALL)
         .style(Style::default());
@@ -24,9 +21,40 @@ pub fn search_ui(f: &mut Frame) {
     ))
     .block(title_block);
 
-    f.render_widget(title, chunks[0])
+    f.render_widget(title, chunks[0]);
+}
+
+pub fn center_panel_ui(f: &mut Frame, chunks: &Rc<[Rect]>) {
+    let center_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(chunks[1]);
+
+    let projects_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let projects_block_title =
+        Paragraph::new(Text::styled("Projects", Style::default().fg(Color::Green)))
+            .block(projects_block);
+
+    let notes_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default());
+
+    let notes_title =
+        Paragraph::new(Text::styled("Notes", Style::default().fg(Color::Green))).block(notes_block);
+
+    f.render_widget(projects_block_title, center_chunks[0]);
+    f.render_widget(notes_title, center_chunks[1]);
 }
 
 pub fn ui(f: &mut Frame, app: &App) {
-    search_ui(f);
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(3), Constraint::Min(1)])
+        .split(f.size());
+
+    search_ui(f, &chunks);
+    center_panel_ui(f, &chunks);
 }
