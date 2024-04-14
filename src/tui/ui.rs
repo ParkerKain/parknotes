1552553d608efd519e5fa4fcbfe9881088699f0a
Parker Kain <parkerjkain@gmail.com>
2024-04-14 19:55:06 -0400

    real titles

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,27 +1,27 @@
 use std::rc::Rc;
 
 use ratatui::{
-    layout::{Constraint, Direction, Layout, Rect},
-    style::{Color, Style},
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Color, Style, Stylize},
     text::Text,
-    widgets::{Block, Borders, Paragraph},
+    widgets::{
+        block::{Position, Title},
+        Block, Borders, Paragraph,
+    },
     Frame,
 };
 
 use super::app::App;
 
 pub fn search_ui(f: &mut Frame, chunks: &Rc<[Rect]>) {
+    let title = Title::from(" Search ".bold());
+
     let title_block = Block::default()
+        .title(title.alignment(Alignment::Left))
         .borders(Borders::ALL)
         .style(Style::default());
 
-    let title = Paragraph::new(Text::styled(
-        "Search for a note",
-        Style::default().fg(Color::Green),
-    ))
-    .block(title_block);
-
-    f.render_widget(title, chunks[0]);
+    f.render_widget(title_block, chunks[0]);
 }
 
 pub fn center_panel_ui(f: &mut Frame, chunks: &Rc<[Rect]>) {
@@ -30,23 +30,21 @@ pub fn center_panel_ui(f: &mut Frame, chunks: &Rc<[Rect]>) {
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
         .split(chunks[1]);
 
+    let projects_block_title = Title::from(" Projects ".bold());
+
     let projects_block = Block::default()
+        .title(projects_block_title.alignment(Alignment::Left))
         .borders(Borders::ALL)
         .style(Style::default());
 
-    let projects_block_title =
-        Paragraph::new(Text::styled("Projects", Style::default().fg(Color::Green)))
-            .block(projects_block);
-
+    let notes_block_title = Title::from(" Notes ".bold());
     let notes_block = Block::default()
+        .title(notes_block_title.alignment(Alignment::Left))
         .borders(Borders::ALL)
         .style(Style::default());
 
-    let notes_title =
-        Paragraph::new(Text::styled("Notes", Style::default().fg(Color::Green))).block(notes_block);
-
-    f.render_widget(projects_block_title, center_chunks[0]);
-    f.render_widget(notes_title, center_chunks[1]);
+    f.render_widget(projects_block, center_chunks[0]);
+    f.render_widget(notes_block, center_chunks[1]);
 }
 
 pub fn ui(f: &mut Frame, app: &App) {
